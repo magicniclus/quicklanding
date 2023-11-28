444111ce2a84691a2cafe600951d4dabbcdf88cc
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-28 11:06:39 +0100

    Create NavBar.tsx

---
+++ src/components/organisms/navigation/NavBar.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import React, { useEffect } from 'react';
+import ButtonPhone from '../../molecules/button/ButtonPhone';
+import { projectData } from '../../../data/projectData';
+import Image from 'next/image';
+
+const navigation = [
+  { name: 'Qui suis-je ?', href: '#qui' },
+  { name: 'Contact', href: '#form' },
+];
+
+const NavBar = () => {
+    return (
+        <header className='text-text'>
+            <nav className="mx-auto flex max-w-5xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+                <div className="flex">
+                    <a href="/" className="-m-1.5 p-1.5">
+                        <span className="sr-only">Trouver-mon-chantier</span>
+                       <Image
+                        layout="responsive"
+                        width={14}
+                        height={36}
+                        src="/logo/logo.png"
+                        alt="trouver-mon-chantier"
+                        style={{ minWidth: '100px' }}
+                        />
+                    </a>
+                </div>
+                <div className='md:block hidden'>
+                    {navigation.map((item, id) => (
+                        <a href={item.href} key={id} className='mr-7'>{item.name}</a>
+                    ))
+                    }
+                </div>
+                {
+                    projectData[0].informations.phone && <ButtonPhone value={projectData[0].informations.phone} bgColor='orange' textColor='text' />
+                }
+            </nav>
+        </header>
+    );
+};
+
+export default NavBar;
