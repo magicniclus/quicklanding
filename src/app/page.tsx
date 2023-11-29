285cc5ceefce8da7ff03e379b520de67f3f29fb8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-29 10:54:56 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import Image from 'next/image'
 import NavBar from '../components/organisms/navigation/NavBar'
+import HeroWithForm from '@/components/organisms/Hero/HeroWithForm'
+import Footer from '@/components/organisms/footer/Footer';
+import Temoignage from '@/components/organisms/commentary/Temoignage';
 
 export default function Home() {
+    const darkComponent = false;
+    
   return (
     <>
       <NavBar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center justify-between px-24 min-w-[100vw]">
+        <HeroWithForm />
+        <Temoignage />
       </main>
+      <Footer />
     </>
   )
 }
