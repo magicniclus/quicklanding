1cb4e84ed8191153b4938e34ecc9e4af2d227473
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-29 10:55:00 +0100

    Update ButtonPhone.tsx

--- src/components/molecules/button/ButtonPhone.tsx
+++ src/components/molecules/button/ButtonPhone.tsx
@@ -1,4 +1,5 @@
 import { PhoneIcon } from "@heroicons/react/24/solid";
+import React from 'react';
 
 // Définition de l'interface pour les props
 interface ButtonPhoneProps {
@@ -8,8 +9,11 @@ interface ButtonPhoneProps {
 }
 
 const ButtonPhone: React.FC<ButtonPhoneProps> = ({ value = '0631420045', bgColor = "orange", textColor = "text" }) => {
+    const bgClass = bgColor === "orange" ? "bg-clear" : "bg-secondary"; // Remplacez 'bg-blue-500' par la classe réelle pour d'autres couleurs
+    const textClass = textColor === "text" ? "text-white" : "text-primary"; // Remplacez 'text-black' par la classe réelle pour d'autres couleurs
+
     return (
-        <a href={`tel:${value}`} className={`py-3 lg:px-5 px-3 rounded-full lg:rounded-lg bg-${bgColor} text-${textColor} flex items-center hover:transition-all hover:duration-100 hover:ease-in-out hover:shadow-lg`}>
+        <a href={`tel:${value}`} className={`py-3 lg:px-5 px-3 rounded-full lg:rounded-lg ${bgClass} ${textClass} flex items-center hover:transition-all hover:duration-100 hover:ease-in-out hover:shadow-lg`}>
             <PhoneIcon className="h-5 w-5 inline-block lg:mr-2" />
             <div className='lg:flex hidden'>
                 {value}
